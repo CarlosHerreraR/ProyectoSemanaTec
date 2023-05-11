79ab40088c18a8155f760ff9ef93bca27234403e
CarlosHerreraR <A01798203@tec.mx>
2023-05-11 16:37:38 -0600

    Documentación memoria

--- memoria.py
+++ memoria.py
@@ -1,7 +1,9 @@
+#Importar librerías
 from random import *
 from turtle import *
 from freegames import path
 
+#Declarar parámetros iniciales
 car = path('car.gif')
 tiles = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N','Ñ', 'O', 'P','Q','R','S','T','U','V','X','Y','Z','#','$','%','&','¿','?'] * 2
 state = {'mark': None}
@@ -9,6 +11,7 @@ hide = [True] * 64
 num_taps = 0  # Variable global para contar el número de taps
 taps_display = Turtle()  # Objeto Turtle para mostrar el número de taps
 
+#Función para trazar la cuadrícula del memorama
 def square(x, y):
     "Draw white square with black outline at (x, y)."
     up()
@@ -21,16 +24,18 @@ def square(x, y):
         left(90)
     end_fill()
 
+#Función para convertir coordenadas en índices de la cuadrícula
 def index(x, y):
     "Convert (x, y) coordinates to tiles index."
     return int((x + 200) // 50 + ((y + 200) // 50) * 8)
 
+#Función para convertir títulos de la cuadrícula en coordenadas
 def xy(count):
     "Convert tiles count to (x, y) coordinates."
     return (count % 8) * 50 - 200, (count // 8) * 50 - 200
 
 num_taps = 0
-
+#Función para contar los taps realizados en el tablero
 def tap(x, y):
     "Update mark and hidden tiles based on tap."
     global num_taps  # Acceder a la variable global
@@ -53,11 +58,12 @@ taps_display = Turtle(visible=False)
 taps_display.penup()
 taps_display.goto(0, 190)
 
-
+#Función mensaje todas las tarjetas abiertas
 def all_open():
     "Check if all tiles are open."
     return all(not tile_hidden for tile_hidden in hide)
 
+#Función para dibujar la imagen y las tarjetas
 def draw():
     "Draw image and tiles."
     clear()
